6060021d47b564ab4bcd4390816eaa432fe3a3de
Xa4 <XavierWelscher@gmx.de>
2026-09-17 18:23:24 +0200

    Added Henyey Greenstein

--- shaders/clouds.glsl
+++ shaders/clouds.glsl
@@ -30,37 +30,53 @@ float getCloudDensity(vec3 p) {
 				return max(finalDensity * u_cloudDensityMultiplier, 0.0);
 }
 
+float dualHenyeyGreenstein(float cosTheta) {
+				float g1 = 0.8;  
+				float g2 = -0.2;
+				float blend = 0.5; 
+
+				float g1Sq = g1 * g1;
+				float g2Sq = g2 * g2;
+
+				float pi = 3.14159265359;
+
+				float phase1 = (1.0 - g1Sq) / (4.0 * pi * pow(1.0 + g1Sq - 2.0 * g1 * cosTheta, 1.5));
+				float phase2 = (1.0 - g2Sq) / (4.0 * pi * pow(1.0 + g2Sq - 2.0 * g2 * cosTheta, 1.5));
+
+				return mix(phase1, phase2, blend);
+}
+
 float calculateLightEnergy(vec3 p, vec3 lightDir) {
-				float lightTransmittance = 1.0;
+				float totalDensity = 0.0;
 				float lightStepSize = 0.5;
 				vec3 lp = p;
 
 				for (int i = 0; i < 6; i++) {
 								lp += lightDir * lightStepSize;
 								float d = getCloudDensity(lp);
+								totalDensity += d * lightStepSize;
 
-								lightTransmittance *= exp(-d * lightStepSize * u_cloudLightAbsorption * 2.0); 
-
-								if (lightTransmittance < 0.01) break;
+								if (totalDensity * u_cloudLightAbsorption > 5.0) break;
 				}
-				return lightTransmittance;
+
+				float beer = exp(-totalDensity * u_cloudLightAbsorption);
+				float powder = 1.0 - exp(-totalDensity * u_cloudLightAbsorption * 2.0);
+
+				return beer * mix(1.0, powder * 2.0, 0.8);
 }
 
 
-void renderClouds(vec3 pos, float step_size, inout float transmittance, inout vec3 color) {
+void renderClouds(vec3 pos, vec3 rd, float step_size, inout float transmittance, inout vec3 accumulated_color) {
 				float density = getCloudDensity(pos);
 
 				if (density > 0.001) {
-								float stepTransmittance = exp(-density * step_size * u_cloudLightAbsorption);
-
-								vec3 lightDir = normalize(vec3(0.8, 0.8, -0.3));
 								float lightEnergy = calculateLightEnergy(pos, lightDir);
-
-								vec3 sunColor = vec3(1.0, 0.95, 0.9);
-								vec3 ambientLight = vec3(0.2, 0.3, 0.4); 
-
-								vec3 scatteredLight = (sunColor * lightEnergy * 1.5 + ambientLight) * density * step_size;
-								color += transmittance * scatteredLight;
+								float cosTheta = dot(rd, lightDir);
+								float phaseVal = dualHenyeyGreenstein(cosTheta);
+								
+								vec3 cloudColor = vec3(1.0, 0.95, 0.9) * lightEnergy * phaseVal * 15.0;
+								float stepTransmittance = exp(-density * step_size * u_cloudLightAbsorption);
+								accumulated_color += transmittance * cloudColor * density * step_size;
 								transmittance *= stepTransmittance;
 				}
 }
